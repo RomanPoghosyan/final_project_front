52c1ebd540dc1e3d7685d65f0578238108794d3b
RomanPoghosyan <roman.poghos@gmail.com>
2020-02-04 13:31:30 +0400

    Created login form with redux-form

--- src/components/Auth/SignIn/SignIn.js
+++ src/components/Auth/SignIn/SignIn.js
@@ -3,18 +3,35 @@ import TextField from "@material-ui/core/TextField";
 import HeaderText from "../../common/HeaderText/HeaderText";
 import useStyles from "../../../utils/styles/useHeaderTextStyle";
 import {Button} from "@material-ui/core";
+import {Field, reduxForm} from "redux-form";
+import {maxLengthCreator, required} from "../../../utils/validators/validators";
+import {renderTextField} from "../../common/FormControlls/FormControlls";
 
+let maxLength15 = maxLengthCreator(15);
+
+const SignInForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit} className={props.className}>
+            <Field label={"Login"} id="standard-basic" name={"login"} component={renderTextField} validate={[required]} />
+            <Field label={"Password"} id="standard-password" name={"password"} component={renderTextField} validate={[required, maxLength15]} />
+            <Button type={"submit"} variant={"contained"} color={"primary"}>Sign In</Button>
+        </form>
+    );
+};
+
+const SignInReduxForm = reduxForm({form: "login"})(SignInForm);
 
 const SignIn = () => {
     const classes = useStyles();
+
+    const onSubmit = (formData) => {
+        console.log(formData);
+    };
+
     return (
         <div className={classes.container}>
             <HeaderText element={<h2>Sign In</h2>}/>
-            <form className={classes.form}>
-                <TextField fullWidth id="standard-basic" label="Login" />
-                <TextField fullWidth id="standard-password" label="Password" />
-                <Button variant={"contained"} color={"primary"}>Sign In</Button>
-            </form>
+            <SignInReduxForm onSubmit={onSubmit} className={classes.form}/>
         </div>
     );
 };
---
+++ src/utils/validators/validators.js
@@ -0,0 +1,11 @@
+export const required = (input) => {
+    if(input) return undefined;
+
+    return "Field is required";
+};
+
+export const maxLengthCreator = (maxLength) => (input) => {
+    if(input.length > maxLength) return `Max length is ${maxLength}`;
+
+    return undefined;
+};
