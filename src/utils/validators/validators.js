b2578f825b5431fb89bfe6bd59f9832f16a86988
tigran <try.tofree@bk.ru>
2020-02-08 00:41:20 +0400

    bug fix

--- src/utils/validators/validators.js
+++ src/utils/validators/validators.js
@@ -12,7 +12,7 @@ export const onlyCharacters = input => {
 
 export const emailChecker = input => {
     const REGEX = /[a-z0-9]+([-+._][a-z0-9]+){0,2}@.*?(\.(a(?:[cdefgilmnoqrstuwxz]|ero|(?:rp|si)a)|b(?:[abdefghijmnorstvwyz]iz)|c(?:[acdfghiklmnoruvxyz]|at|o(?:m|op))|d[ejkmoz]|e(?:[ceghrstu]|du)|f[ijkmor]|g(?:[abdefghilmnpqrstuwy]|ov)|h[kmnrtu]|i(?:[delmnoqrst]|n(?:fo|t))|j(?:[emop]|obs)|k[eghimnprwyz]|l[abcikrstuvy]|m(?:[acdeghklmnopqrstuvwxyz]|il|obi|useum)|n(?:[acefgilopruz]|ame|et)|o(?:m|rg)|p(?:[aefghklmnrstwy]|ro)|qa|r[eosuw]|s[abcdeghijklmnortuvyz]|t(?:[cdfghjklmnoprtvwz]|(?:rav)?el)|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw])\b){1,2}/;
-    if (!input.test(REGEX)) {
+    if (!REGEX.test(input)) {
         return "Enter valid email address"
     }
 };
