c3d1d09ec1e0dbbefd56f5fa79bc298363371a95
kamoklekchyan <kamoklekchyan@gmail.com>
2020-02-09 20:17:52 +0400

    added app-reducer.test.js and auth-reducer.test.js

--- src/redux/app-reducer.test.js
+++ src/redux/app-reducer.test.js
@@ -2,13 +2,21 @@ import React from "react";
 import ReactDOM from 'react-dom'
 import appReducer, {initializedSuccess} from "./app-reducer";
 import {render} from "@testing-library/react";
-import App from "../App";
 
-test('INITIALIZED_SUCCESS is true', () => {
+test('initialized is true', () => {
     let action = initializedSuccess();
     let initialState = {
         initialized: false
     };
     let newState = appReducer(initialState, action);
-    expect(newState.initialized).toBeTruthy()
+    expect(newState.initialized).toBe(true);
+});
+
+test('initialized is false', () => {
+    let action = {};
+    let initialState = {
+        initialized: false
+    };
+    let newState = appReducer(initialState, action);
+    expect(newState.initialized).toBe(false);
 });
