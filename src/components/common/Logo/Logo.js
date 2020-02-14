c001c1241080e7e28a2c0bd2f3e448441471f50e
RomanPoghosyan <roman.poghos@gmail.com>
2020-02-14 16:29:36 +0400

    fixed error with logo link

--- src/components/common/Logo/Logo.js
+++ src/components/common/Logo/Logo.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from '../../../assets/images/logoMedium.png';
-import {Link} from "react-router-dom";
 
 function Logo() {
     return (
-        <Link to={"/"}>
+        <div>
             <img src={logo} width={"100%"} height={'100%'} alt={"Company Logo"} title={"Go to home page"} />
-        </Link>
+        </div>
     )
 }
 
